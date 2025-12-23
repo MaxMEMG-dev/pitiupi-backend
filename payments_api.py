3c69a31fe3f26c6248879eb8ef5852512ee36ac7
MaxMEMG-dev <memgcorporation@gmail.com>
2025-12-23 12:21:02 -0500

    Update payments_api.py

--- payments_api.py
+++ payments_api.py
@@ -136,6 +136,7 @@ async def pay_redirect(
                 "success_url": "https://t.me/pitiupibot",
                 "failure_url": "https://t.me/pitiupibot",
                 "pending_url": "https://t.me/pitiupibot",
+                "review_url": "https://t.me/pitiupibot"
             },
             "billing_address": {
                 "street": street,
